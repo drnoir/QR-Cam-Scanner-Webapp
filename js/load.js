5fd8a81051f6434531cfa43aacb43716a083dfe4
drnoir <chrispgodber@gmail.com>
2019-08-28 13:56:10 +0100

    updated load js, removed opacity on css
    modifed - works now added google tracking for analytics

--- js/load.js
+++ js/load.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-  // let now = new Date().toLocaleTimeString();
+  let now = new Date().toLocaleTimeString();
   
-  // // fade in the interface slowly
-  // window.onload = function(){
-  //     $( ".bg-interface" ).animate({
-  //         opacity: 1
-  //       }, 1000, function() {
-  //         $("#boot")[0].play();
-  //         $( "span.time" ).append(now);
-  //   });
+  // fade in the interface slowly
+  window.onload = function(){
+      // $( ".bg-interface" ).animate({
+      //     opacity: 1
+      //   }, 1000, function() {
+      //     $("#boot")[0].play();
+          $( "span.time" ).append(now);
+    }
   
   let scanner = new Instascan.Scanner({ video: document.getElementById('videoElement') });
         scanner.addListener('scan', function (content) {
